34c3d2aa4ba27d6a771c7795868dae9330a88d89
codergalib20 <coderboygalif@gmail.com>
2023-04-29 01:19:08 +0600

    readme documention update

--- src/pages/api/middleware/authenticateUser.ts
+++ src/pages/api/middleware/authenticateUser.ts
@@ -1,3 +1,19 @@
-import jwt from 'jsonwebtoken';
-import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import User from "@/models/User";
 
+interface DecodedToken {
+  userId: string;
+  iat: number;
+  exp: number;
+}
+
+/**
+ * Middleware function to check whether the user is authenticated.
+ * If the user is authenticated, the authenticated user object is attached to the request object (req.user).
+ * If the user is not authenticated, a 401 Unauthorized response is sent.
+ */
+
+export async function authenticateUser(req: NextApiRequest, res: NextApiResponse, next: () => void) {
+    
+}
