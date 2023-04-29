c5d3ea8f73ca40f51efc9dbad5c2a1bca3257427
codergalib20 <coderboygalif@gmail.com>
2023-04-29 20:55:29 +0600

    stacks

--- src/components/stack.tsx
+++ src/components/stack.tsx
@@ -1,10 +1,11 @@
 import { stack } from '@/fakeData/stacks';
 import Container from './container';
+import Image from 'next/image';
 
 const Stack = () => {
     return (
         <Container>
-            <div className={`grid grid-cols-12 w-full max-w-4xl mx-auto`}>
+            <div className={`grid grid-cols-12 w-full max-w-4xl mx-auto py-[200px]`}>
                 <div className={`col-span-8 border-[1px] p-4 border-[#778899] bg-[#ececec] rounded`}>
                     <h3 className='text-2xl font-bold pb-3 text-[#000000]'>My Stack</h3>
                     <div className='border border-[#778899] bg-[#ffffff] p-4 rounded grid grid-cols-3'>
@@ -15,6 +16,9 @@ const Stack = () => {
                     </div>
                 </div>
                 <div className={`col-span-4 border-[1px] p-4 border-[#778899] bg-[#ececec] relative top-10 -left-12`}>
+                    <div className='absolute left-0 bottom-32 w-full h-full'>
+                        <Image fill src="/shape/earth.png" alt='stack image' />
+                    </div>
                     <h3 className='text-2xl font-bold pb-3 text-[#000000]'>Exploring</h3>                    <div className='border border-[#778899] bg-[#ffffff] p-4 rounded'>
                         {stack.map((skill: { status: string[], name: string }, index) => {
                             return skill?.status.includes('exploring') && <div key={index}>{skill?.name}</div>
