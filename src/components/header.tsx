cc980b2bad40ba1d044310ef15b269dec60a0082
codergalib20 <coderboygalif@gmail.com>
2023-04-29 22:42:13 +0600

    stacks

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,23 @@
-const Header : React.FC =() => {
+import Container from "./container";
+
+const Header: React.FC = () => {
     return (
-        <div className="bg-white">
-            Header
+        <div
+            className="bg-white shadow-md py-2"
+            style={{ boxShadow: "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px" }}
+        >
+            <Container>
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </Container>
         </div>
     )
 }
