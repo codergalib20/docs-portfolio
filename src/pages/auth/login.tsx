0044e2224e9d26e694d2b8675fa93017fdfd7398
codergalib20 <coderboygalif@gmail.com>
2023-04-29 16:51:13 +0600

    commited

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const LoginPage = () => {
-    const [username, setUsername] = useState('');
+    const [email, setemail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
@@ -13,18 +13,18 @@ const LoginPage = () => {
         <div className="flex items-center justify-center h-screen">
             <div className="w-full max-w-md">
                 <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleLogin}>
-                    <h2 className="text-2xl text-center font-bold mb-6">Log In</h2>
+                    <h2 className="text-2xl text-center font-bold mb-6 text-red-400">Log In</h2>
                     <div className="mb-4">
-                        <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
-                            Username
+                        <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+                            E-mail
                         </label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="username"
+                            id="email"
                             type="text"
-                            placeholder="Username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
+                            placeholder="E-mail"
+                            value={email}
+                            onChange={(e) => setemail(e.target.value)}
                             required
                         />
                     </div>
