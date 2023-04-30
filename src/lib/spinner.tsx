14e4da8f4e12a564da686ce16bd23b30aa07b101
codergalib20 <coderboygalif@gmail.com>
2023-04-30 20:04:49 +0600

    deleted some functions

--- src/lib/spinner.tsx
+++
@@ -1,8 +0,0 @@
-export default function Spinner() {
-    return <div className="spinner">
-        <div className="dot dot1"></div>
-        <div className="dot dot2"></div>
-        <div className="dot dot3"></div>
-    </div>
-
-}
