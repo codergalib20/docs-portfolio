54b9622ae7ad2a30b21334105348630e00f82109
codergalib20 <coderboygalif@gmail.com>
2023-04-30 20:09:21 +0600

    deleted some functions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,8 @@
-import { useAuth } from '@/hooks/useAuth';
 import '@/styles/globals.css'
-import { getUserById } from '@/utils/database';
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
-import { useEffect } from 'react';
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  // console.log(sessionStorage.getItem('token')!)
-  // const [user, setUser] = useState(null);
-  const { user } = useAuth();
-  console.log(user);
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        // const user = await getUserById( )
-      }
-      catch (err) { console.error(err) }
-    }
-  }, [])
 
   return (
     <div className='app-bg h-screen'>
