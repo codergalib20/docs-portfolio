cc77a0e56888cbb7f8e8a8cdfd7c147445245ded
codergalib20 <coderboygalif@gmail.com>
2023-04-30 21:46:09 +0600

    deleted some functions

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+
+
 const Login: React.FC = () => {
 
     const title: string = 'Welcome to Galib\'s World'
@@ -56,20 +59,20 @@ const Login: React.FC = () => {
                             >Remember me</label>
                         </div>
                     </div>
-                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">htmlForgot password?</a>
+                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                 </div>
                 <div>
                     <button
                         type="submit"
-                        className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                        className="my-4 w-full text-white bg-[#fa6985] hover:bg-[#f55776] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                         Sign in
                     </button>
-                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                    <p className="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
                         Don’t have an account yet?
-                        <a href="#"
-                            className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                        <Link href="/signup" legacyBehavior><a
+                            className="font-medium text-[#fa6985] hover:underline dark:text-primary-500">
                             Sign up
-                        </a>
+                        </a></Link>
                     </p>
                 </div>
             </div>
