13469ad5037111be41b35d0c587fae91ed481a67
codergalib20 <coderboygalif@gmail.com>
2023-04-30 23:38:03 +0600

    deleted some functions]

--- src/fakeData/menus.ts
+++ src/fakeData/menus.ts
@@ -1,3 +1,5 @@
+import { AiFillHome } from "react-icons/ai";
+
 type Data = {
   name: string;
   icon: string;
@@ -10,7 +12,7 @@ type Data = {
 const menus: Data[] = [
   {
     name: "Home",
-    icon: "icon",
+    icon: ``,
   },
   {
     name: "About",
@@ -23,6 +25,46 @@ const menus: Data[] = [
       {
         name: "About 2",
         icon: "icon2",
+      },
+    ],
+  },
+  {
+    name: "blogs",
+    icon: "blogicon",
+    sub: [
+      {
+        name: "blog-1",
+        icon: "blogicon-1",
+      },
+      {
+        name: "blog-2",
+        icon: "blogicon-2",
+      },
+      {
+        name: "blog-3",
+        icon: "blogicon-3",
+      },
+      {
+        name: "blog-4",
+        icon: "blogicon-4",
+      },
+    ],
+  },
+  {
+    name: "docs",
+    icon: "docs",
+    sub: [
+      {
+        name: "docs 1",
+        icon: "docs icon",
+      },
+      {
+        name: "docs 2",
+        icon: "docs icon",
+      },
+      {
+        name: "docs 3",
+        icon: "docs icon",
       },
     ],
   },
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,16 @@
+import menus from "@/fakeData/menus";
+
 export default function Dashboard() {
     return (
         <div>
-            dashboard
+            <div className="flex h-screen">
+                <div className="w-[260px] bg-[#9257fd] h-full p-4 ">
+                    {menus.map((item, index) => (
+                        <div key={index}>{item.name} {item.icon}</div>
+                    ))}
+                </div>
+                <div></div>
+            </div>
         </div>
     )
 }
