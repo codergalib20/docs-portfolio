57776abbe887e194274b70f888fd34aa0d9bbb3c
codergalib20 <coderboygalif@gmail.com>
2023-04-30 20:33:00 +0600

    deleted some functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Header from "@/components/header";
-import Hero from "@/components/hero";
-import Stack from "@/components/stack";
+import Header from "@/@core/components/header";
+import Hero from "@/@core/components/hero";
+import Stack from "@/@core/components/stack";
 
 const Home = () => {
   return (
