09e01dcaed22ae1c691eec6c027971736e4dffc3
codergalib20 <coderboygalif@gmail.com>
2023-05-03 06:54:28 +0600

    commited

---
+++ src/pages/api/posts.ts
@@ -0,0 +1,12 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+
+const handler: NextApiHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      return res.status(200).json({ hello: "hello" });
+    default:
+      return res.status(404).json({ message: "this is an error message" });
+  }
+};
