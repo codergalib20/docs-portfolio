34881feda2c3681a7c9ebfee995a9a7c2163b52e
codergalib20 <coderboygalif@gmail.com>
2023-05-06 20:04:35 +0600

    documentation blogger

---
+++ src/layout/dashboardLayout.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+interface Props {
+    children: React.ReactNode;
+};
+
+const dashboardLayout: FC<Props> = () => {
+    return (
+        <div>
+            dashboardLayout
+        </div>
+    )
+};
+
+export default dashboardLayout
