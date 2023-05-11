ca7693ae0db47cd5b277481a6b7afdc1ba92a537
codergalib20 <coderboygalif@gmail.com>
2023-05-11 21:24:50 +0600

    commited

---
+++ src/pages/admin/dashboard.tsx
@@ -0,0 +1,17 @@
+import Sidebar from "@/@core/dashboard/sidebar";
+import { NextPage } from "next";
+
+interface Props { };
+
+const Dashboard: NextPage<Props> = () => {
+    return (
+        <div className="m-0 font-sans text-base antialiased font-normal leading-default bg-gray-50 text-slate-500 w-full h-screen flex">
+            <div className="w-full max-w-[250px] h-full overflow-y-scroll relative">
+                <Sidebar />
+            </div>
+            <div className="w-full"></div>
+        </div>
+    )
+};
+
+export default Dashboard
