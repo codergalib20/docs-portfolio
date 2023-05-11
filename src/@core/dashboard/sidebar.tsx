5ddca325d8b429bdbc62dc1169aa56372b957472
codergalib20 <coderboygalif@gmail.com>
2023-05-11 22:17:15 +0600

    commited

--- src/@core/dashboard/sidebar.tsx
+++ src/@core/dashboard/sidebar.tsx
@@ -13,10 +13,10 @@ const Sidebar: FC<Props> = () => {
     return (
         <motion.aside initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }} className="ease-nav-brand z-990 inset-y-0 my-4 ml-4 block w-full -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-red-500 p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 h-full overflow-y-scroll bg-transparent">
+            exit={{ opacity: 0 }} className="ease-nav-brand z-990 inset-y-0 my-4 ml-4 block w-full -translate-x-full flex-wrap items-center justify-between rounded-2xl border-0 bg-[#332C39] overflow-y-scroll p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 h-full bg-transparent">
             <div className="h-19.5">
-                <i className="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden" sidenav-close></i>
-                <a className="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700" href="javascript:;" target="_blank">
+                <i className="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times xl:hidden" sidenav-close></i>
+                <a className="block px-8 py-6 m-0 text-sm whitespace-nowrap text-white" href="javascript:;" target="_blank">
                     <Image width="20" height="20" src="/assets/img/logo-ct.png" className="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8" alt="main_logo" />
                     <span className="ml-1 font-semibold transition-all duration-200 ease-nav-brand">Soft UI Dashboard</span>
                 </a>
@@ -27,27 +27,9 @@ const Sidebar: FC<Props> = () => {
             <div className="py-4 overflow-y-scroll items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
                 <ul className="flex flex-col pl-0 mb-0">
                     <li className="mt-3 w-full">
-                        <Link href={mainUrl} className={`py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg ${!query.tab ? "bg-white" : ''} px-4 font-semibold text-slate-700 transition-colors`}>
-                            <div className="bg-gradient-to-tl from-purple-700 to-pink-500 shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
-                                <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                                    <title>shop</title>
-                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                        <g transform="translate(-1716.000000, -439.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                                            <g transform="translate(1716.000000, 291.000000)">
-                                                <g transform="translate(0.000000, 148.000000)">
-                                                    <path
-                                                        className="opacity-60"
-                                                        d="M46.7199583,10.7414583 L40.8449583,0.949791667 C40.4909749,0.360605034 39.8540131,0 39.1666667,0 L7.83333333,0 C7.1459869,0 6.50902508,0.360605034 6.15504167,0.949791667 L0.280041667,10.7414583 C0.0969176761,11.0460037 -1.23209662e-05,11.3946378 -1.23209662e-05,11.75 C-0.00758042603,16.0663731 3.48367543,19.5725301 7.80004167,19.5833333 L7.81570833,19.5833333 C9.75003686,19.5882688 11.6168794,18.8726691 13.0522917,17.5760417 C16.0171492,20.2556967 20.5292675,20.2556967 23.494125,17.5760417 C26.4604562,20.2616016 30.9794188,20.2616016 33.94575,17.5760417 C36.2421905,19.6477597 39.5441143,20.1708521 42.3684437,18.9103691 C45.1927731,17.649886 47.0084685,14.8428276 47.0000295,11.75 C47.0000295,11.3946378 46.9030823,11.0460037 46.7199583,10.7414583 Z"
-                                                    ></path>
-                                                    <path
-                                                        className=""
-                                                        d="M39.198,22.4912623 C37.3776246,22.4928106 35.5817531,22.0149171 33.951625,21.0951667 L33.92225,21.1107282 C31.1430221,22.6838032 27.9255001,22.9318916 24.9844167,21.7998837 C24.4750389,21.605469 23.9777983,21.3722567 23.4960833,21.1018359 L23.4745417,21.1129513 C20.6961809,22.6871153 17.4786145,22.9344611 14.5386667,21.7998837 C14.029926,21.6054643 13.533337,21.3722507 13.0522917,21.1018359 C11.4250962,22.0190609 9.63246555,22.4947009 7.81570833,22.4912623 C7.16510551,22.4842162 6.51607673,22.4173045 5.875,22.2911849 L5.875,44.7220845 C5.875,45.9498589 6.7517757,46.9451667 7.83333333,46.9451667 L19.5833333,46.9451667 L19.5833333,33.6066734 L27.4166667,33.6066734 L27.4166667,46.9451667 L39.1666667,46.9451667 C40.2482243,46.9451667 41.125,45.9498589 41.125,44.7220845 L41.125,22.2822926 C40.4887822,22.4116582 39.8442868,22.4815492 39.198,22.4912623 Z"
-                                                    ></path>
-                                                </g>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </svg>
+                        <Link href={mainUrl} className={`py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg ${!query.tab ? "bg-white text-[#332C39]" : ''} px-4 font-semibold text-white transition-colors`}>
+                            <div className="bg-gradient-to-tl shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5 text-green-500">
+                                D
                             </div>
                             <span className="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Dashboard</span>
                         </Link>
@@ -55,8 +37,8 @@ const Sidebar: FC<Props> = () => {
 
                     {/* Buttons============================= */}
                     <li className="mt-3 w-full">
-                        <Link href={`${mainUrl}?tab=blogs`} className={`${query.tab === 'blogs' && "bg-white"} py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors`}>
-                            <div className={`shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5 ${query.tab === 'blogs' && "bg-white"}`}>
+                        <Link href={`${mainUrl}?tab=blogs`} className={`${query.tab === 'blogs' && "bg-white  text-[#332C39]"} py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors`}>
+                            <div className={`shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5 bg-white`}>
                                 <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg">
                                     <title>office</title>
                                     <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -71,12 +53,12 @@ const Sidebar: FC<Props> = () => {
                                     </g>
                                 </svg>
                             </div>
-                            <span className="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Blogs</span>
+                            <span className="ml-1 duration-300 pointer-events-none ease-soft">Blogs</span>
                         </Link>
                     </li>
 
                     <li className="mt-3 w-full">
-                        <Link href={`${mainUrl}?tab=documents`} className={`${query.tab === 'documents' && "bg-white"} py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors`}>
+                        <Link href={`${mainUrl}?tab=documents`} className={`${query.tab === 'documents' && "bg-white  text-[#332C39]"} py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors`}>
                             <div className="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
                                 <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" >
                                     <title>credit-card</title>
