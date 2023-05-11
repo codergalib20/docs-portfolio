8805b7a4d69761ed095bd3a51a9f917642d56f7d
codergalib20 <coderboygalif@gmail.com>
2023-05-11 21:49:17 +0600

    commited

---
+++ src/@core/dashboard/pages/default.tsx
@@ -0,0 +1,13 @@
+import {FC} from "react";
+
+interface Props {};
+
+const DefaultPage :FC<Props> = () => {
+    return (
+         <div>
+             DefaultPage
+         </div>
+    )
+};
+
+export default DefaultPage
--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,3 +1,6 @@
+import BlogsDashboard from "@/@core/dashboard/pages/blogs";
+import DefaultPage from "@/@core/dashboard/pages/default";
+import DocumentDashboard from "@/@core/dashboard/pages/documents";
 import Sidebar from "@/@core/dashboard/sidebar";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
@@ -6,14 +9,18 @@ interface Props { };
 
 const Dashboard: NextPage<Props> = () => {
     const { query } = useRouter() || {}
-
-
+    
+    
     return (
-        <div className="m-0 font-sans text-base antialiased font-normal leading-default bg-gray-50 text-slate-500 w-full h-screen flex">
+        <div className="m-0 font-sans text-base antialiased font-normal leading-default bg-gray-50 text-slate-500 w-full h-screen flex gap-4">
             <div className="w-full max-w-[250px] h-full overflow-y-scroll relative">
                 <Sidebar />
             </div>
-            <div className="w-full"></div>
+            <div className="w-full h-full overflow-y-scroll">
+                {!query.tab && <DefaultPage />}
+                {query.tab === 'blogs' && <BlogsDashboard />}
+                {query.tab === 'documents' && <DocumentDashboard />}
+            </div>
         </div>
     )
 };
